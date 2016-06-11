d797badef392967d543eb5fea6b8aaefd1ea9c32
yeluo-ls <yeluo_ls@126.com>
2016-06-11 18:09:57 +0800

    mc_webapp

--- mc_webapp/js/common.js
+++ mc_webapp/js/common.js
@@ -1,12 +1,53 @@
-function loaded(){
-	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-
-	var myScroll = new IScroll('#wrapper',{
-		momentum: false
-	});
+var Main = Main || {};
+Main = {
+	scroll : null,
+	swiper : null,
+	currentPage : '#index',
+	loaded : function(){
+		var me = this;
+		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+		me.initWrapper();
+	},
 	
-	var mySwiper = new Swiper ('.swiper-container', {
-	    loop: true,
-	    autoplay:5000
-	})   
+	init : function(){
+		var me = this;
+		me.initIndex();
+	},
+	
+	initWrapper : function(){
+		var me = this;
+		if(me.scroll){
+			me.scroll.destroy();
+		}
+		me.scroll = null;
+		var wrapper = me.currentPage + ' .wrapper';
+		me.scroll = new IScroll(wrapper,{
+			momentum: false,
+			tap:true
+		});
+	},
+	
+	initIndex : function(){
+		var me = this;
+		$('#index .lts').on('tap',function(){
+			me.currentPage = '#lottery';
+		    me.switchPage();
+		});
+		
+		me.swiper = new Swiper('.swiper-container', {
+		    loop: true,
+		    autoplay:5000
+		});
+	},
+	
+	switchPage : function(){
+		var me = this;
+		me.initWrapper();
+		$('.pageCurrent').removeClass('pageCurrent');
+	    $(me.currentPage).addClass('pageCurrent');
+	}
 };
+
+(function($){
+	Main.init();
+})(Zepto);
